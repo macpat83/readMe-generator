23670e1210bfab0c42d18085ca60cf25018e915e
macpat83 <macpat@gmail.com>
2022-05-13 10:22:12 -0500

    edits to generate page

--- page-template.js
+++ page-template.js
@@ -1,27 +1,33 @@
 function generateReadme (data) {
     return `# ${data.title}
 
-## Project Links
-* Deployed Link ${data.deployedlink}
-* Repo Link ${data.repolink}
-
-## Description
-${data.description}
-
 ## Table of Contents
 * [Installation](#installation)
 * [Usage](#usage)
 * [Credits](#credits)
 * [License](#license)
 
+
+## Project Links
+* Deployed Link ${data.deployedlink}
+* Repo Link ${data.repolink}
+
+
+## Description
+${data.description}
+
+
 ## Built With
 ${data.languages}
+
 
 ## Future Development
 ${data.development}
 
+
 ## Contributors
 ${data.contributors}
+
 
 ${data.img}
 
